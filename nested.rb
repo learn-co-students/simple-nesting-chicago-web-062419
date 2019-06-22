c75b2fda6a4db0260cfefa9b5d85ad07c348bb6a
Erica Hawkins <ehawk901@gmail.com>
2019-06-22 18:27:32 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+        programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-end
+       programmer_hash[:alan_kay][:known_for]
+  end 
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -78,10 +79,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } 
+     
+    programmer_hash[0]= {:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]}}
 
-    
-end
+ end 
 
 def changing_alan
 	programmer_hash = 
@@ -101,6 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+      programmer_hash["Object Orientation"] = alans_new_info
      
      
 end
